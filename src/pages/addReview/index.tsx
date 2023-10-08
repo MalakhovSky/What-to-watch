6a77de86dfeb49b960f9e1ca1a0654c6692bb8c4
MalakhovSky <tviler6@gmail.com>
2023-10-08 20:53:38 +0300

    fixed setRating func

--- src/pages/addReview/index.tsx
+++ src/pages/addReview/index.tsx
@@ -48,38 +48,38 @@ export const AddReview = props => {
         </div>
 
         <div className="add-review">
-          <form action="#" className="add-review__form">
-            <div className="rating">
-              <div className="rating__stars" >
+          <form  action="#" className="add-review__form">
+            <div  className="rating">
+              <div  onChange={(e)=>setRating(e)} className="rating__stars" >
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
+                <input  className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
                 <label className="rating__label" htmlFor="star-1">Rating 1</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
+                <input  className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
                 <label className="rating__label" htmlFor="star-2">Rating 2</label>
 
-                <input onClick={(e)=>setRating(e)}  className="rating__input" id="star-3" type="radio" name="rating" value="3" />
+                <input   className="rating__input" id="star-3" type="radio" name="rating" value="3" />
                 <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
+                <input  className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
                 <label className="rating__label" htmlFor="star-4">Rating 4</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
+                <input  className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
                 <label className="rating__label" htmlFor="star-5">Rating 5</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-6" type="radio" name="rating" value="6"/>
+                <input  className="rating__input" id="star-6" type="radio" name="rating" value="6"/>
                 <label className="rating__label" htmlFor="star-6">Rating 6</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-7" type="radio" name="rating" value="7"/>
+                <input  className="rating__input" id="star-7" type="radio" name="rating" value="7"/>
                 <label className="rating__label" htmlFor="star-7">Rating 7</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-8" type="radio" name="rating" value="8" />
+                <input  className="rating__input" id="star-8" type="radio" name="rating" value="8" />
                 <label className="rating__label" htmlFor="star-8">Rating 8</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-9" type="radio" name="rating" value="9"/>
+                <input  className="rating__input" id="star-9" type="radio" name="rating" value="9"/>
                 <label className="rating__label" htmlFor="star-9">Rating 9</label>
 
-                <input onClick={(e)=>setRating(e)} className="rating__input" id="star-10" type="radio" name="rating" value="10"/>
+                <input  className="rating__input" id="star-10" type="radio" name="rating" value="10"/>
                 <label className="rating__label" htmlFor="star-10">Rating 10</label>
               </div>
             </div>
