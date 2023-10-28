024f8f0fdc833725d40f9761c327376d0572d670
MalakhovSky <tviler6@gmail.com>
2023-10-28 20:33:04 +0300

    add commentsSlice it store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filmsSliceReducer from "./features/filmsSlice";
+import commentsReducer from "./features/commentsSlice";
 
 export const store = configureStore({
   reducer: {
     films:filmsSliceReducer,
+    comments:commentsReducer,
   },
 })
 
