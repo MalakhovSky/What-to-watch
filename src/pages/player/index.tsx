cffeb8690d16d65d52c0d3e45c60ae39c21ff465
MalakhovSky <tviler6@gmail.com>
2023-10-29 21:35:32 +0300

    add toggleVideo player func

--- src/pages/player/index.tsx
+++ src/pages/player/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {useAppDispatch, useAppSelector} from "../../redux/hooks/useAppDispatch";
+import {fetchFilms} from "../../redux/features/asyncActions";
+import {useParams} from "react-router-dom";
 
 export const Player = () => {
+  const [isPlay, setIsPlay] = useState(false);
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchFilms())
+  }, [dispatch]);
+
+  const {id} = useParams()
+  const filmsData = useAppSelector(state=>state.films.films)
+
+  const film = filmsData.find((item)=>item.id === Number(id))
+  console.log(film,'film')
+
+  const playerRef = useRef(null);
+  const toggleVideo = () => {
+
+    if(isPlay){
+      playerRef.current.pause()
+    }else{
+      playerRef.current.play()
+    }
+    setIsPlay(!isPlay)
+  }
+
+  if(film){
   return (
 
     <div className="player">
-      <video src="#" className="player__video" poster="img/player-poster.jpg"></video>
+      <video className="player__video" poster={film.poster_image} ref={playerRef} >
+        <source src={film.video_link}
+
+                type="video/mp4"/>
+      </video>
 
       <button type="button" className="player__exit">Exit</button>
 
@@ -14,14 +47,17 @@ export const Player = () => {
             <progress className="player__progress" value="30" max="100"></progress>
             <div className="player__toggler" style={{left:" 30%"}}>Toggler</div>
           </div>
-          <div className="player__time-value">1:30:29</div>
+          <div className="player__time-value">{film.run_time}Фораматировать</div>
         </div>
 
         <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use href="#play-s"></use>
-            </svg>
+          <button onClick={()=>toggleVideo()}
+                  type="button" className="player__play" >
+            {
+              !isPlay ? <svg viewBox="0 0 19 19" width="19" height="19">
+                <use href="#play-s"></use>
+              </svg>:<span>PAUSE</span>
+            }
             <span>Play</span>
           </button>
           <div className="player__name">Transpotting</div>
@@ -37,4 +73,7 @@ export const Player = () => {
     </div>
 
   );
+  } else{
+    return <div>LOADING</div>
+  }
 };
