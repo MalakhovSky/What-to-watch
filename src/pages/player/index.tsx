511361235b048716d73e916559dd621d5773f211
MalakhovSky <tviler6@gmail.com>
2023-10-29 21:44:55 +0300

    add play - pause render logic

--- src/pages/player/index.tsx
+++ src/pages/player/index.tsx
@@ -56,7 +56,11 @@ export const Player = () => {
             {
               !isPlay ? <svg viewBox="0 0 19 19" width="19" height="19">
                 <use href="#play-s"></use>
-              </svg>:<span>PAUSE</span>
+              </svg>:<svg width="14px" height="21px" viewBox="0 0 14 21" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <polygon id="Line" fill="#EEE5B5" fill-rule="nonzero" points="0 -1.11910481e-13 4 -1.11910481e-13 4 21 0 21"></polygon>
+                <polygon id="Line" fill="#EEE5B5" fill-rule="nonzero" points="10 -1.11910481e-13 14 -1.11910481e-13 14 21 10 21"></polygon>
+              </g></svg>
+
             }
             <span>Play</span>
           </button>
