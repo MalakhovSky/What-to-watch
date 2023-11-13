cebc0173159a7d4d53880d5363e239e13ff94097
MalakhovSky <tviler6@gmail.com>
2023-11-13 13:52:25 +0300

    filter genres

--- src/components/catalogGenresList/index.tsx
+++ src/components/catalogGenresList/index.tsx
@@ -6,18 +6,28 @@ export const CatalogGenresList = ({films}) => {
   useEffect(() => {
   }, []);
 
-
+  if(films){
+    films = films.filter((value, index, self) =>
+        index === self.findIndex((t) => (
+          t.genre === value.genre
+        ))
+    )
+  }
 
   return (
     <div>
       <ul className="catalog__genres-list">
 
             <li className="catalog__genres-item catalog__genres-item--active">
-              <a href="/src/pages#" className="catalog__genres-link">All genres</a>
+              <div className="catalog__genres-link">All genres</div>
             </li>
-
-
-
+        {
+          films?.map((item)=>(
+            <li className="catalog__genres-item">
+              <div className="catalog__genres-link">{item.genre}</div>
+            </li>
+          ))
+        }
 
       </ul>
     </div>
