85425fd935111e13f837f0a0f10b3a83a38c48fc
MalakhovSky <tviler6@gmail.com>
2023-11-20 18:30:12 +0300

    add Sign in tests

---
+++ cypress/e2e/App.e2e/App.cy.js
@@ -0,0 +1,10 @@
+describe('Log in',()=>{
+  it("Sign in",()=>{
+    cy.visit("http://localhost:3000/login")
+    cy.get('[data-cy="email"]').should('have.text','')
+    cy.get('[data-cy="password"]').should('have.text','')
+    cy.get('[data-cy="email"]').type('abc@gmail.com')
+    cy.get('[data-cy="password"]').type('123')
+    cy.get('[data-cy="button"]').click()
+  })
+})
